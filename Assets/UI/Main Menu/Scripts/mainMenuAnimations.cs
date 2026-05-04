f4a2e56b94c5e73afc7fbe91c65b8554e905a223
Sgtryan10 <sgtryan123456@gmail.com>
2026-05-04 17:31:02 -0400

    Progress on main menu UI with transitions using UIToolkit and .svgs my beloved

---
+++ Assets/UI/Main Menu/Scripts/mainMenuAnimations.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using System.Threading.Tasks;
+
+public class mainMenuAnimations : MonoBehaviour
+{
+    public int staggerDelayMs = 500;
+
+    async void OnEnable()
+    {
+        var uiDoc = GetComponent<UIDocument>();
+        var root = uiDoc.rootVisualElement;
+
+        var divider = root.Q<VisualElement>("Divider");
+        var container = root.Q<VisualElement>("ButtonContainer");
+
+        // Initial Delay
+        await Task.Delay(2000);
+
+        // Title Image
+        var logoCurtain = root.Q<VisualElement>("TitleImageContainer");
+
+
+        if (logoCurtain != null)
+        {
+            logoCurtain.AddToClassList("curtain-parent-open");
+        }
+
+        await Task.Delay(500);
+
+        // Divider
+        if (divider != null)
+        {
+            divider.AddToClassList("divider-end");
+        }
+
+        if (container == null) return;
+
+        await Task.Delay(500);
+
+        // Buttons
+        foreach (var button in container.Children())
+        {
+            button.AddToClassList("fade-end");
+            await Task.Delay(staggerDelayMs);
+        }
+    }
+}
